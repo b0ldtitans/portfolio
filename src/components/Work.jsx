90f55c4a6b4b6da910cceb3f9fa66013022fc392
Thareq Muhammad Azani <thareqazani@gmail.com>
2023-10-28 23:09:05 +0700

    commit

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -88,25 +88,24 @@ const Work = () => {
                 </div>
               </div>
             </a>
-            {/* image */}
-            <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
-              {/* overlay */}
+
+            {/* <div className="group relative overflow-hidden border-2 border-white/50 rounded-xl">
               <div className="group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300"></div>
-              {/* img */}
+
               <img
                 className="group-hover:scale-125 transition-all duration-500"
                 src={porto3}
                 alt=""
               />
-              {/* pretitle */}
+
               <div className="absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50">
                 <span className="text-gradient">UI/UX Design</span>
               </div>
-              {/* title */}
+
               <div className="absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50">
                 <span className="text-3xl text-white ">Project Title</span>
               </div>
-            </div>
+            </div> */}
           </motion.div>
         </div>
       </div>
